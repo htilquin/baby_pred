50627c78ce5de41d520e97c0c423165f04366a81
Hélène Tilquin <58738324+htilquin@users.noreply.github.com>
2021-09-27 22:01:20 +0200

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,255 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+import matplotlib.pyplot as plt
+import altair as alt
+from textwrap import wrap
+from wordcloud import WordCloud
+from PIL import Image
+
+from utils import *
+
+path_to_df = "processed_data.pkl"
+
+st.markdown("# ✨ Team Crevette ✨")
+st.write("~~")
+st.markdown("Bienvenue sur la page de visualisation de vos pronostics !")
+
+df = pd.read_pickle(path_to_df)
+df["date"] = pd.to_datetime(df["date"], dayfirst=True)
+
+### VARIABLES ALTAIR
+with st.container():
+    source = df
+    sexes = ['Fille', 'Garçon']
+    colors_sex = ['#1FC3AA', 'darkorange']
+    color_scale = alt.Scale(domain=sexes, range=colors_sex)
+    selector = alt.selection_single(empty='all', fields=['sexe'])
+    condition_color = alt.condition(selector, 'sexe:N',
+                        alt.value('lightgray'), scale=color_scale,
+                        legend=alt.Legend(title="", labelFontSize=12, orient="right"))
+    base = alt.Chart(source).add_selection(selector).interactive()
+
+## INTRO + SEXE
+with st.container():
+    col1, col2 = st.columns(2)
+
+    with col1:
+        st.write("~~")
+        st.write("Vous avez été {} personnes à participer.". format(len(df)))
+        st.markdown("**Merci ! 🤗**")
+        st.write("Vous trouverez sur cette page un petit récap de vos pronostics...")
+        st.write("""&nbsp;  
+        Pour rappel, la personne avec le plus de points* remportera le poids (réel!) du bébé
+        en chocolats et/ou bonbons, selon sa préférence.  
+        &nbsp;&nbsp;🍬 &nbsp; 🍫 &nbsp; 🍭
+
+        * barême à suivre... 🧮     
+        """)
+
+    ### SEXE 
+    with col2 :
+        fig, ax = plt.subplots(figsize=(4,4))
+        ax.pie(df['sexe'].value_counts(), 
+                labels=sexes, labeldistance=1.15,
+                wedgeprops = { 'linewidth' : 3, 'edgecolor' : 'white' },
+                autopct='%1.1f%%',
+                colors=colors_sex)
+        ax.axis('equal')
+        st.pyplot(fig)
+
+### DATE DE NAISSANCE
+with st.container():
+    st.markdown("&nbsp;")
+    st.markdown("## 📆 &nbsp; Date de naissance")
+    st.markdown("&nbsp;")
+    st.write("✨ Zoom et filtre garçon/fille possible.")
+    st.markdown("🪧 Terme : **12 février 2022** (en gris clair).")
+
+    ticks = base.mark_tick(thickness=2, size=80, opacity=0.5).encode(
+        alt.X('date:T', title="Date prédite"),
+        color=condition_color,
+        tooltip=[
+            alt.Tooltip('date', title="Date prédite "),
+            alt.Tooltip('heure', title="à "),
+            alt.Tooltip('prenom', title="par "),
+            alt.Tooltip('nom', title=" "),
+        ],
+    ).properties(height=135).interactive()
+
+    d_day = alt.Chart(pd.DataFrame({
+        'Date de naissance' : pd.date_range(start="2022-02-11T23:00:00", end="2022-02-12T22:59:59", periods=100)
+    })).mark_tick(
+        thickness=2, size=80, color='lightgrey', opacity=1
+    ).encode(x='Date de naissance:T').interactive()
+
+    st.altair_chart(
+        alt.layer(d_day, ticks),
+        use_container_width=True
+    )
+
+### Jour et heure de naissance ?
+
+
+### TAILLE + POIDS
+with st.container():
+    st.markdown('## 📏 &nbsp; Mensurations...')
+    st.markdown("&nbsp;")
+    st.write("✨ Zoom et filtre garçon/fille possible.")
+
+    taille_scale = alt.Scale(domain=(40, 60))
+    poids_scale = alt.Scale(domain=(2000, 5000))
+    tick_axis = alt.Axis(labels=False, ticks=False)
+    area_args = {'opacity': .3, 'interpolate': 'step'}
+
+    points = base.mark_circle().encode(
+        alt.X('taille', scale=taille_scale),
+        alt.Y('poids', scale=poids_scale),
+        color=condition_color,
+        tooltip=[
+            alt.Tooltip('poids', title="Poids"), 
+            alt.Tooltip('taille', title='Taille'), 
+            alt.Tooltip('prenom', title="par "),
+            alt.Tooltip('nom', title=" ")
+        ]
+    )
+
+    top_chart = base.mark_area(**area_args).encode(
+        alt.X('taille:Q',
+            bin=alt.Bin(maxbins=20, extent=taille_scale.domain),
+            stack=None,
+            title=''
+            ),
+        alt.Y('count()', stack=None, title=''),
+        color=condition_color,
+        tooltip=[alt.Tooltip('taille', title='Taille'), alt.Tooltip('count()', title='Nb ')]
+    ).properties(height=80).transform_filter(selector)
+
+    right_chart = base.mark_tick().encode(
+        alt.X('sexe', axis=tick_axis, title=''),
+        alt.Y('poids', axis=tick_axis, scale=poids_scale, title=''),
+        tooltip=alt.Tooltip('poids'),
+        color=condition_color
+    )
+
+    st.altair_chart(top_chart & (points | right_chart), use_container_width=True)
+# ajouter les poids/taille de Hélène et Flo ?
+
+### CAPILARITE
+with st.container():
+    st.markdown("## 💈 &nbsp; Et les cheveux ?")
+
+    ordre_cheveux = ['Maxi chevelure !', 'Chevelure classique', 'Juste assez pour ne pas être chauve...', 'Pas de cheveux !']
+    ordre_couleur = ['Noirs', 'Bruns', 'Roux', 'Blonds', 'Pas de cheveux... Pas de cheveux !'] 
+    couleurs_cheveux = ['black', 'saddlebrown', 'darkorange', 'gold', 'lightgray']
+    longueur_cheveux = [4, 2, 0.5, 0.1]
+
+    width, radii, angles, theta, colors, nb_labels = make_bars_specifics(
+        df, longueur_cheveux, ordre_cheveux, couleurs_cheveux, ordre_couleur)
+
+    fig = plt.figure(figsize=(8,8))
+    ax = fig.add_axes([0, 0, 0.8, 0.8], polar=True)
+    bottom = 5
+    bars = ax.bar(theta, radii, width=(np.array(width)-0.01), bottom=bottom, color=colors)
+
+    # mise en forme du graphique
+    ax.set_thetamin(0)
+    ax.set_thetamax(180)
+    ax.set_xticks(angles)
+    ax.set_xticklabels(["\n".join(wrap(r, 11, break_long_words=False)) for r in ordre_cheveux])
+    ax.set_yticks([0, 10])
+    ax.set_yticklabels([])
+    ax.xaxis.grid(False)
+    ax.yaxis.grid(False)
+    ax.spines["start"].set_color("none")
+    ax.spines["end"].set_color("none")
+    ax.spines["polar"].set_color("none")
+
+    # affichage des labels + rotation pour lisibilité et opacité
+    rotations = np.rad2deg(theta)
+    for x, bar, rotation, label in zip(theta, bars, rotations, nb_labels):
+        new_rotation = 180 + rotation if rotation > 90 else rotation
+        lab = ax.text(x,bottom+bar.get_height()+1, label, size=7,
+                        ha='center', va='center', rotation=new_rotation, rotation_mode="anchor")  
+        bar.set_alpha(0.7) 
+        
+    st.pyplot(fig)
+
+### PRENOM
+with st.container():
+    st.markdown("## ✏️ &nbsp; Et enfin... le prénom !")
+    st.markdown("&nbsp;")
+    st.markdown("Plus un prénom est écrit en gros, plus vous avez été nombreux à miser dessus 😉")
+
+    dict_masc = dict(df.prenom_masc.value_counts())
+    dict_fem = dict(df.prenom_fem.value_counts())
+
+    dict_both = {
+        k: dict_masc.get(k, 0) + dict_fem.get(k, 0) 
+        for k in set(dict_masc) | set(dict_fem)
+    }
+
+    mask = np.array(Image.open("cloud-icon.png"))
+    mask[mask == 0] = 255
+
+    color_to_words = { colors_sex[0] : list(dict_fem.keys()) }
+    grouped_color_func = GroupedColorFunc(color_to_words, default_color = colors_sex[1])
+
+    wordcloud = WordCloud(
+        random_state=35,
+        background_color = "white", 
+        width=1000, height=600,
+        max_font_size=40,
+        mask=mask,
+        relative_scaling=1,
+        color_func=grouped_color_func,
+    ).generate_from_frequencies(dict_both)
+
+    fig = plt.figure(figsize=(16,9))
+    plt.imshow(wordcloud, interpolation='bilinear')
+    plt.axis("off")
+
+    st.pyplot(fig)
+
+## Fille et garçon séparé !
+with st.container() :
+    col1, col2 = st.columns(2)
+
+    mask_thunder = np.array(Image.open("thunder.png"))
+    mask_thunder[mask_thunder == 0] = 255
+
+    with col1 :
+        wordcloud = WordCloud(
+            random_state=35,
+            background_color = "white", 
+            max_font_size=40,
+            mask=mask_thunder,
+            color_func=get_single_color_func(colors_sex[0]),
+        ).generate_from_frequencies(dict_fem)
+
+        fig = plt.figure(figsize=(16,9))
+        plt.imshow(wordcloud, interpolation='bilinear')
+        plt.axis("off")
+
+        st.pyplot(fig)
+
+    with col2 :
+        wordcloud = WordCloud(
+            random_state=35,
+            background_color = "white", 
+            max_font_size=40,
+            mask=mask_thunder,
+            color_func=get_single_color_func(colors_sex[1]),
+        ).generate_from_frequencies(dict_masc)
+
+        fig = plt.figure(figsize=(16,9))
+        plt.imshow(wordcloud, interpolation='bilinear')
+        plt.axis("off")
+
+        st.pyplot(fig)
+
+# FOOTER
+st.write("### &nbsp;")
+st.markdown("##### Fait avec 💖 par Hélène.")
+
---
+++ utils.py
@@ -0,0 +1,99 @@
+import numpy as np
+from wordcloud import get_single_color_func
+
+def calcul_angles(angles, proportion, acc):
+    current_angle = proportion * np.pi
+    acc.append(current_angle)
+    if len(angles) == 0:
+        angles.append(current_angle/2)
+    else :
+        angles.append(angles[-1] + acc[-2]/2 + current_angle/2)
+
+    return angles, acc
+
+def make_bars_specifics(df, longueur_cheveux, ordre_cheveux, couleurs_cheveux, ordre_couleur) :
+
+    width = []
+    radii = []
+    angles = []
+    acc = []
+    theta = []
+    colors = []
+    nb_labels = []
+
+    for longueur in ordre_cheveux :
+        nb = len(df.loc[(df['longueur_cheveux'] == longueur)])
+        angles, acc = calcul_angles(angles, nb/len(df), acc)
+        
+        for couleur in ordre_couleur:
+            nb = len(df.loc[(df['longueur_cheveux'] == longueur) & (df['couleur_cheveux'] == couleur)])
+            if nb > 0 :
+                nb_labels.append("{} - {}".format(couleur, nb))
+                radii.append(longueur_cheveux[ordre_cheveux.index(longueur)])
+                colors.append(couleurs_cheveux[ordre_couleur.index(couleur)])
+                theta, width = calcul_angles(theta, nb/len(df), width)
+    
+    # modif label pour cause de texte trop long !
+    new_labels = []
+    for texte in nb_labels :
+        new_label = texte.replace('Pas de cheveux... Pas de cheveux !', 'Chauve !')
+        new_labels.append(new_label)
+
+    return width, radii, angles, theta, colors, new_labels
+
+class SimpleGroupedColorFunc(object):
+    """Create a color function object which assigns EXACT colors
+       to certain words based on the color to words mapping
+       Parameters
+       ----------
+       color_to_words : dict(str -> list(str))
+         A dictionary that maps a color to the list of words.
+       default_color : str
+         Color that will be assigned to a word that's not a member
+         of any value from color_to_words.
+    """
+
+    def __init__(self, color_to_words, default_color):
+        self.word_to_color = {word: color
+                              for (color, words) in color_to_words.items()
+                              for word in words}
+
+        self.default_color = default_color
+
+    def __call__(self, word, **kwargs):
+        return self.word_to_color.get(word, self.default_color)
+
+
+class GroupedColorFunc(object):
+    """Create a color function object which assigns DIFFERENT SHADES of
+       specified colors to certain words based on the color to words mapping.
+       Uses wordcloud.get_single_color_func
+       Parameters
+       ----------
+       color_to_words : dict(str -> list(str))
+         A dictionary that maps a color to the list of words.
+       default_color : str
+         Color that will be assigned to a word that's not a member
+         of any value from color_to_words.
+    """
+
+    def __init__(self, color_to_words, default_color):
+        self.color_func_to_words = [
+            (get_single_color_func(color), set(words))
+            for (color, words) in color_to_words.items()]
+
+        self.default_color_func = get_single_color_func(default_color)
+
+    def get_color_func(self, word):
+        """Returns a single_color_func associated with the word"""
+        try:
+            color_func = next(
+                color_func for (color_func, words) in self.color_func_to_words
+                if word in words)
+        except StopIteration:
+            color_func = self.default_color_func
+
+        return color_func
+
+    def __call__(self, word, **kwargs):
+        return self.get_color_func(word)(word, **kwargs)
